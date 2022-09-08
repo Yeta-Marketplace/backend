782baabeef1700801eb84d5a6ef6f061c5e5f9dd
Mathemmagician <ramil9898@gmail.com>
2022-09-08 18:40:42 -0400

    updated alembic

--- app/app/models.py
+++ app/app/models.py
@@ -1,24 +1,24 @@
 
 from typing import Optional
+from pydantic import EmailStr
 from sqlmodel import Field, SQLModel
 
 
 class UserBase(SQLModel):
+    email: EmailStr = Field(index=True)
     full_name: Optional[str]
-    email: str = Field(index=True)
+    is_active: bool = True
 
 
 class User(UserBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     full_name: str = Field(index=True)
     hashed_password: str
-    is_active: bool
     is_superuser: bool
 
 
 class UserCreate(UserBase):
     password: str
-    is_active: bool = True
 
 
 class UserRead(UserBase):
@@ -26,4 +26,6 @@ class UserRead(UserBase):
 
 
 class UserUpdate(SQLModel):
-    password: str
+    full_name: Optional[str]
+    password: Optional[str]
+    is_active: Optional[bool]
