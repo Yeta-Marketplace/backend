d418d79f6cab5dfdb26a19300b98600fae307633
Mathemmagician <ramil9898@gmail.com>
2022-09-07 21:58:22 -0400

    chaning api_v1 to v1

--- app/app/main.py
+++ app/app/main.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-from app.api.api_v1.api import api_router
+from app.api.v1.api import api_router
 from app.core.config import settings
 
 
